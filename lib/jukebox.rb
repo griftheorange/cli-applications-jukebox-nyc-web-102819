3fc7f63ad851067a73c08376ac7743d405ba3b11
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 05:00:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,26 @@
-# Add your code here
+def help
+  puts 'I accept the following commands:'
+  puts '- help : displays this help message'
+  puts '- list : displays a list of songs you can play'
+  puts '- play : lets you choose a song to play'
+  puts '- exit : exits this program'
+end
+
+def list(songs)
+  songs.each_with_index{ |song, index|
+    puts "#{index+1}. #{song}"
+  }
+end
+
+def play(songs)
+  puts 'Please enter a song name or number:'
+  input = gets.strip
+  
+  songs.length.times {|i|
+    if input.to_i == i || input == songs[i]
+      puts #{songs[i]}"
+      return
+    end
+  }
+  puts "Invalid input, please try again"
+end
